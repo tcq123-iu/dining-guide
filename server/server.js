36bb0bf12c20d250efe598f4027b9fe8f5e042f2
tcq123-iu <125744700+tcq123-iu@users.noreply.github.com>
2024-05-09 17:32:11 +0700

    update api route

--- server/server.js
+++ server/server.js
@@ -1,12 +1,37 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express =require('express');
-
-
-const app=express();
+const express = require("express");
 
 
-const port= process.env.PORT
-app.listen(port,()=>{
+const app = express();
+
+//get all restaurants
+app.get("/api/v1/restaurants", (req, res) => {
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      restaurants: ["Ngo Quyen", "KFC"],
+
+    },
+
+  });
+});
+
+//get a restaurant
+app.get("/api/v1/restaurants/:restaurantid", (req, res)=>{
+  console.log(req.params);
+
+});
+
+//create a restaurant
+app.post("/api/v1/restaurants", (req, res)=>{
+  console.log(req);
+
+});
+
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
   console.log(`at ${port}`);
 });
