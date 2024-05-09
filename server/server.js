4a4370880abcab396a32b7e1dba8b40b6b315a98
tcq123-iu <125744700+tcq123-iu@users.noreply.github.com>
2024-05-09 16:10:03 +0700

    setup

---
+++ server/server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const express =require('express');
+
+
+const app=express();
+
+
+const port= process.env.PORT
+app.listen(port,()=>{
+  console.log(`at ${port}`);
+});
