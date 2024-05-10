dd44264fe78c6da138138f77c9d541b6947f3eb9
tcq123-iu <125744700+tcq123-iu@users.noreply.github.com>
2024-05-10 16:50:21 +0700

    Add function for backend

--- server/server.js
+++ server/server.js
@@ -13,71 +13,115 @@ app.use(express.json())
 
 //get all restaurants
 app.get("/api/v1/restaurants", async (req, res) => {
+  try {
+    const results = await db.query("SELECT * FROM restaurants");
+    console.log(results);
 
-  const result = await db.query("SELECT * FROM restaurants");
-  console.log(result);
+    res.status(200).json({
+      status: "success",
+      results: results.rows.length,
+      data: {
+        restaurants: results.rows,
 
-  res.status(200).json({
-    status: "success",
-    data: {
-      restaurants: ["Ngo Quyen", "KFC"],
+      },
 
-    },
+    });
+  } catch (err) {
+    console.log(err);
 
-  });
+  }
+
+
+
 });
 
 //get a restaurant
-app.get("/api/v1/restaurants/:id", (req, res) => {
-  console.log(req.params);
+app.get("/api/v1/restaurants/:id", async (req, res) => {
+  console.log(req.params.id);
 
-  res.status(200).json({
-    status: "success",
-    data: {
+  try {
+    const results = await db.query("select * from restaurants where id = $1", [req.params.id]);
 
-      restaurants: "Ngo quyen",
+    res.status(200).json({
+      status: "success",
+      data: {
 
-    },
+        restaurants: results.rows[0],
 
-  });
+      },
+
+    });
+
+  } catch (err) {
+    console.log(err);
+  }
+
 
 
 });
 
 //create a restaurant
-app.post("/api/v1/restaurants", (req, res) => {
+app.post("/api/v1/restaurants", async (req, res) => {
   console.log(req.body);
-  res.status(201).json({
-    status: "success",
-    data: {
+  try {
+    const results = await db.query(
+      "insert into restaurants(name, location, price_range) values ($1, $2,$3) returning *",
+      [req.body.name, req.body.location, req.body.price_range]);
 
-      restaurants: "Ngo quyen",
+    res.status(201).json({
+      status: "success",
+      data: {
 
-    },
+        restaurants: results.rows[0],
 
-  });
+      },
+
+    });
+  } catch (err) {
+    console.log(err);
+
+  }
+
 
 });
 
 //update restaurant
-app.put("/api/v1/restaurants/:id", (req, res) => {
+app.put("/api/v1/restaurants/:id", async (req, res) => {
   console.log(req.params.id);
   console.log(req.body);
-  res.status(200).json({
-    status: "success",
-    data: {
 
-      restaurants: "Ngo quyen",
+  try {
+    const results = await db.query("update restaurants set name = $1, location = $2, price_range = $3 where id = $4 returning *",
+      [req.body.name, req.body.location, req.body.price_range, req.params.id]);
 
-    },
+    res.status(200).json({
+      status: "success",
+      data: {
 
-  });
+        restaurants: results.rows[0],
+
+      },
+
+    });
+
+  } catch (err) {
+    console.log(err);
+  }
+
 
 });
 
 //delete restaurant
-app.delete("/api/v1/restaurants/:id", (req, res) => {
+app.delete("/api/v1/restaurants/:id", async (req, res) => {
+
+  try {
+    const results = await db.query(" delete from restaurants where id =$1", [req.params.id]);
+  } catch (err) {
+    console.log(err);
+
+  }
   res.status(204).json({
+
     status: "success",
 
   });
