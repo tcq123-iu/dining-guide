459475302586b744b61d961d8e92ab93cead1f5c
tcq123-iu <125744700+tcq123-iu@users.noreply.github.com>
2024-05-10 14:26:57 +0700

    Finish route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const { restart } = require("nodemon");
 
 
 const app = express();
@@ -21,14 +22,59 @@ app.get("/api/v1/restaurants", (req, res) => {
 });
 
 //get a restaurant
-app.get("/api/v1/restaurants/:restaurantid", (req, res) => {
+app.get("/api/v1/restaurants/:id", (req, res) => {
   console.log(req.params);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+
+      restaurants: "Ngo quyen",
+
+    },
+
+  });
+
 
 });
 
 //create a restaurant
 app.post("/api/v1/restaurants", (req, res) => {
   console.log(req.body);
+  res.status(201).json({
+    status: "success",
+    data: {
+
+      restaurants: "Ngo quyen",
+
+    },
+
+  });
+
+});
+
+//update restaurant
+app.put("/api/v1/restaurants/:id", (req, res) => {
+  console.log(req.params.id);
+  console.log(req.body);
+  res.status(201).json({
+    status: "success",
+    data: {
+
+      restaurants: "Ngo quyen",
+
+    },
+
+  });
+
+});
+
+//delete restaurant
+app.delete("/api/v1/restaurants/:id", (req, res) => {
+  res.status(204).json({
+    status: "success",
+
+  });
 
 });
 
